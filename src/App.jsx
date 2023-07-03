db3cedc4009843f327fff58a03a1688a3d7395a8
Aida Chernyavskaya <aidachernyavskaya@gmail.com>
2023-07-03 13:17:13 +0300

    Add actions with redux hooks - useDisoath, useSelector

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,30 @@
 import './App.css';
+import {useDispatch, useSelector} from "react-redux";
 
 function App() {
-  return (
+    const dispath = useDispatch();
+    const cash = useSelector(state => state.cash);
+    console.log(cash);
+
+    const addCash = (cash) => {
+        dispath({type: 'ADD_CASH', payload: cash});
+    }
+
+    const getCash = (cash) => {
+        dispath({type: 'GET_CASH', payload: cash});
+    }
+
+    return (
     <div className="App">
-      Working
+        <div style={{fontSize: '30px', margin: '80px 0 0 0', textAlign: 'center'}}>
+            {cash}
+        </div>
+        <div style={{display: 'flex'}}>
+          <button onClick={() => addCash(Number(prompt()))}>Пополнить счет</button>
+          <button onClick={() => getCash(Number(prompt()))}>Снять со счета</button>
+        </div>
     </div>
-  );
+    );
 }
 
 export default App;
